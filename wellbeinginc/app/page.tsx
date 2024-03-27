b2ae2a5152280955dfa4a2031b66e6ad1e2831d1
ec221148 <ec221148@qmul.ac.uk>
2024-03-27 21:01:39 +0000

    Updated css landing

--- wellbeinginc/app/page.tsx
+++ wellbeinginc/app/page.tsx
@@ -1,10 +1,23 @@
+import Image from 'next/image';
+import './landing.css'
+import logo from '../components/images/logo.png';
 import AuthButton from "../components/AuthButton";
 
 export default async function Index() {
 
   return (
     <>
-      <AuthButton />
+      <div className="section">
+        <h1>Welcome to Wellbeing Inc.</h1>
+        <h3>Powered by FDM</h3>
+        <div className='center'>
+          <Image src={logo} alt="logo" />
+        </div>
+        <p>Wellbeing Inc. is a wellbeing and mental health web-based application for FDM employees</p>
+        <div className='center'>
+          <AuthButton />
+        </div>
+      </div>
     </>
   );
 }
