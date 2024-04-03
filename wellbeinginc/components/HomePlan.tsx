65e29468283658d2ddd693a6efe7c1c104ffcf9e
Newton Nath <n.nath@se22.qmul.ac.uk>
2024-04-03 03:49:17 +0100

    Modified plan and edit plan section in homepage

--- wellbeinginc/components/HomePlan.tsx
+++ wellbeinginc/components/HomePlan.tsx
@@ -1,7 +1,34 @@
+import './HomePlan.css';
 export default function HomePlan() {
     return (
         <>
-            <h1>Home Plan</h1>
+            <div className='plan-container'>
+                <h1>YOUR PLAN</h1>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className='name-cell'>BREAKFAST</td>
+                            <td className='data-container'></td>
+                        </tr>
+                        <tr>
+                            <td className='name-cell'>SNACK</td>
+                            <td className='data-container'></td>
+                        </tr>
+                        <tr>
+                            <td className='name-cell'>LUNCH</td>
+                            <td className='data-container'></td>
+                        </tr>
+                        <tr>
+                            <td className='name-cell'>DINNER</td>
+                            <td className='data-container'></td>
+                        </tr>
+                        <tr>
+                            <td className='name-cell'>EXERCISE</td>
+                            <td className='data-container'></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
--- wellbeinginc/components/HomeProfile.tsx
+++ wellbeinginc/components/HomeProfile.tsx
@@ -1,6 +1,9 @@
 'use client'
 import { useState } from 'react';
 import Modal from './ProfileModal';
+import Image from 'next/image'
+import blank from './images/Blank Profile Picture.jpg'
+import './HomeProfile.css'
 
 const HomeProfile = () => {
     const [modalOpen, setModalOpen] = useState(false);
@@ -15,10 +18,20 @@ const HomeProfile = () => {
 
     return (
         <>
-            <div>
-                <button onClick={openModal}>Edit Profile</button>
-                <Modal isOpen={modalOpen} onClose={closeModal} />
-            </div>
+          <div className='container'>
+            <div className='profile-photo'>
+                <Image src={blank} alt="Blank" height="100" width="100"/>
+              </div>
+              <div className='details'>
+                  <h3>Name: Anna Perker</h3>
+                  <p>Email: annaperker@gmail.com</p>
+                  <p>Status: FDM Employee</p>
+              </div>
+              <div className='edit-profile'>
+                  <button onClick={openModal}>Edit Profile</button>
+                  <Modal isOpen={modalOpen} onClose={closeModal} />
+              </div>
+          </div>
         </>
     )
 }
