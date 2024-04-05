a242a583d6e94f13cd9bc32b115bdcf741558f09
Melek Dudzinski <ec221148@qmul.ac.uk>
2024-04-05 21:06:43 +0100

    First login set profile

--- wellbeinginc/app/protected/page.tsx
+++ wellbeinginc/app/protected/page.tsx
@@ -9,6 +9,7 @@ import HomeArticle from "@/components/HomeArticle"
 import HomePlan from "@/components/HomePlan"
 import HomeProfile from "@/components/HomeProfile"
 import Chatbot from "@/components/Chatbot";
+import SetProfile from '@/components/SetProfileModal';
 import './protected.css';
 
 
@@ -27,35 +28,45 @@ export default async function ProtectedPage() {
     return redirect("/login");
   }
 
-  const { data, error } = await supabase.from('TestUserProfile').select().eq('EmployeeNo', user.id);
+  const { data, error } = await supabase.from('TestUserProfile').select('*').eq('EmployeeNo', user.id);
+  console.log(user.id);
+  console.log(data)
 
   if (error) {
     console.log("Error getting queue status");
   }
 
   if (data.length > 0) {
+    console.log("Profile exists")
     profileSet = true;
   }
 
   return (
     <>
-      <Navbar activePage={activePage}/>
-      <div className="homepage-grid">
-        <div className="profile-sect"><HomeProfile /></div>
-        <div id="plan-sect"><HomePlan /></div>
-        <div id ="top-bit-container">
-          <div id="home-chatbot-section"></div>
-          <h1>HELLO ANNA,</h1>
-          <p>Need someone to talk to? Connect with our Mental Health Champion for</p>
-          <p>confidential support and guidance</p>
-          <button id = "chat-button">CONNECT</button>
+    {profileSet ? (
+      <div>
+        <Navbar activePage={activePage}/>
+        <div className="homepage-grid">
+          <div className="profile-sect"><HomeProfile /></div>
+          <div id="plan-sect"><HomePlan /></div>
+          <div id ="top-bit-container">
+            <div id="home-chatbot-section"></div>
+            <h1>HELLO ANNA,</h1>
+            <p>Need someone to talk to? Connect with our Mental Health Champion for</p>
+            <p>confidential support and guidance</p>
+            <button id = "chat-button">CONNECT</button>
+          </div>
+          <div id="articles-sect">
+            <HomeArticle />
+            <button id='see-more-button'><Link href="/articles">See More</Link></button>
+          </div>
+          <Chatbot userID={user.id}/>
         </div>
-        <div id="articles-sect">
-          <HomeArticle />
-          <button id='see-more-button'><Link href="/articles">See More</Link></button>
-        </div>
-        <Chatbot userID={user.id}/>
       </div>
+    ) : (
+      <SetProfile />
+    )}
+      
     </>
   );
 }
