1584e1a2d9d92aa3417504e72cd169498a094fd3
Melek-Dudzinski <120277839+Melek-Dudzinski@users.noreply.github.com>
2024-04-08 17:56:21 +0100

    Revert "Buttons"

--- wellbeinginc/components/HomeChatbot.tsx
+++ wellbeinginc/components/HomeChatbot.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react';
+import { createClient } from "@supabase/supabase-js"
 import Chatbot from "@/components/Chatbot";
-import ChatbotPrototype from "@/components/ChatbotPrototype";
 import SupabaseClient from '@/components/Supabase';
 import '@/app/protected/protected.css'
 
@@ -10,6 +10,7 @@ type HomeChatbotProps = {
 }
 
 const HomeProfile = (props: HomeChatbotProps) => {
+  // const supabase = createClient('https://nwysqtnfikxauolsknzt.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53eXNxdG5maWt4YXVvbHNrbnp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NDUwNjAsImV4cCI6MjAyNjAyMTA2MH0.P7FqiOhrxAGqukCFe98sMDp0kq8deBHv_PLSsYr0Cko');
   const [userName, setUserName] = useState([]);
   const [load, setLoad] = useState(false);
 
@@ -40,8 +41,7 @@ const HomeProfile = (props: HomeChatbotProps) => {
                     <p>Need someone to talk to? Connect with our Mental Health Champion for</p>
                     <p>confidential support and guidance</p>
                     {/* <button id = "chat-button">CONNECT</button> */}
-                    {/* <Chatbot userID={props.userID} userRole={name.Role}/> */}
-                    <ChatbotPrototype userID={props.userID} userRole={name.Role}/>
+                    <Chatbot userID={props.userID} userRole={name.Role}/>
                 </div>
             ))}
 
